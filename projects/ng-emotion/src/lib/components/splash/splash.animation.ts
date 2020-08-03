3a15cf3582a387fc4c522b1ef5852e0bd7016988
dannymcgee <dannymcgee@gmail.com>
2020-08-03 18:40:29 -0400

    Updated splash animation to define void state

--- projects/ng-emotion/src/lib/components/splash/splash.animation.ts
+++ projects/ng-emotion/src/lib/components/splash/splash.animation.ts
@@ -1,10 +1,11 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import { Anim, EaseOut } from '../../utils';
 
 export const splash = trigger('splash', [
+	state('void', style({ opacity: 1, transform: 'scale(0)' })),
+
 	transition(':enter', [
-			style({ opacity: 1, transform: 'scale(0)' }),
 			// TODO: Make animation duration configurable
 			animate(`${Anim.Duration.Long}ms ${EaseOut.Sine}`,
 					style({ opacity: 0, transform: 'scale(1.333)' }),

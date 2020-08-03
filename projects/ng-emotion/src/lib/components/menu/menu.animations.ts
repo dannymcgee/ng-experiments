c2965b04e9dc0ee4f2e7a780b2265dbc7c117da3
dannymcgee <dannymcgee@gmail.com>
2020-08-03 16:15:14 -0400

    Fixed menu animation breaking the position strategy

--- projects/ng-emotion/src/lib/components/menu/menu.animations.ts
+++ projects/ng-emotion/src/lib/components/menu/menu.animations.ts
@@ -2,7 +2,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Anim, EaseOut } from '../../utils';
 
 export const actionMenu = trigger('actionMenu', [
-	state('void', style({ height: 0, opacity: 0, transform: 'scale(0.667)' })),
-	state('*', style({ height: '*', opacity: 1, transform: 'scale(1)' })),
-	transition('void => *', animate(`${Anim.Duration.Short}ms ${EaseOut.Cubic}`)),
+	state('void', style({ opacity: 0, transform: 'scale(0.5, 0)' })),
+	state('*', style({ opacity: 1, transform: 'scale(1)' })),
+	transition('void => *', animate(`${Anim.Duration.Medium}ms ${EaseOut.Cubic}`)),
 ]);
